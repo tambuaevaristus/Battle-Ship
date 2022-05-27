f8fe0c3aa648ded1cfda9dcbba333ccd943f10e4
Tambua Evaristus <evaristustambua@gmail.com>
2022-05-27 13:24:45 +0100

    mvc and sound working properly

--- js/main.js
+++ js/main.js
@@ -1,67 +1,136 @@
+// ************
+// Model
+// ***********
+
 const rows = 7;
 const cols = 7;
-const table = document.getElementById("table");
-let form = document.getElementById("form");
-form.addEventListener("submit", play)
-
+var shipsSunk = 0;
+var numberOfGuess = 0;
+var shipLocations = [];
 const y_Axis = ["A", "B", "C", "D", "E", "F", "G"];
 
+const table = document.getElementById("table");
+let form = document.getElementById("inputForm");
+form.addEventListener("submit", play);
+
+
+
+
+
+
+
+// ************
+// View   //
+// ***********
+
+function displayMessage(message) {
+  var messageArea = document.getElementById("messageArea");
+  messageArea.innerHTML = message;
+}
+
+function displayHit(location) {
+  var cell = document.getElementById(location);
+  cell.setAttribute("class", "hit");
+  var audio = new Audio("/assets/mixkit-martial-arts-fast-punch-2047.wav");
+  audio.play();
+}
+
+function displayMiss(location) {
+  var cell = document.getElementById(location);
+  cell.setAttribute("class", "miss");
+  var audio = new Audio("/assets/mixkit-technological-futuristic-hum-2133.wav");
+  audio.play();
+}
+
+function displayWin(message) {
+  var messageArea = document.getElementById("messageArea");
+  messageArea.innerHTML = message;
+  var audio = new Audio("/assets/mixkit-stadium-crowd-light-applause-362.wav");
+  audio.play();
+}
+
+// Create imaginary grid on the board
 function createGrid() {
   for (let i = 0; i < rows; i++) {
     const tr = document.createElement("tr");
     let x = y_Axis[i];
     for (let j = 0; j < cols; j++) {
       const cell = document.createElement("td");
-      cell.setAttribute("class", "cell");
-      cell.setAttribute("id", x+""+j)
+      cell.setAttribute("id", x + "" + j);
       tr.appendChild(cell);
     }
     table.appendChild(tr);
   }
 }
 
-function loadShips(){
-    
-    let locationIdArray = []
+// Display ship location on the board
+function loadShips() {
+  for (let i = 0; i < 15; i++) {
+    let randomRow = Math.floor(Math.random() * 7);
+    let randomCol = y_Axis[Math.floor(Math.random() * 7)];
 
-for (let i = 0; i <3; i++){
-    let randomRow = Math.floor(Math.random() *7)
-    let randomCol = y_Axis[Math.floor(Math.random() *7)]
+    let locationId = randomCol + "" + randomRow;
 
     // make sure a  location does not repeat
-    let locationId = randomCol+""+randomRow
+    if (shipLocations.includes(locationId) == false) {
+      shipLocations.push(locationId);
+    }
+    if (shipLocations.length == 4) {
+      return;
+    }
 
-    // if(locationIdArray.includes(locationId)){
-    
-    // }{
-        locationIdArray.push(locationId);
-        console.log(locationIdArray);
-        document.getElementById(locationId).style.backgroundImage = "url(/assets/ship.png)"
-        document.getElementById(locationId).style.display = "none"
-    // }
-    
+    shipLocations.map((location) => {
+      document.getElementById(location).setAttribute("data-state", "1");
+      document.getElementById(location).style.backgroundColor = "red";
+      // displayHit(location)
+    });
+  }
 }
 
+
+
+
+
+// ***************
+// Controller Methods
+//****************** */
+
+function play(e) {
+  e.preventDefault();
+  const inputGuest = document.getElementById("input").value;
+
+  const cell = document.getElementById(inputGuest);
+
+  if (cell.className == "miss") {
+    displayMessage("You Seleted this cell already!!!  Try again ");
+  } else if (cell.className == "hit") {
+    displayMessage("You Destroyed this ship already!!!  Try again ");
+  } else {
+    numberOfGuess++;
+    if (shipsSunk == 2) {
+      displayHit(inputGuest);
+      displayWin(
+        "Congrats You Sinked 3 ships after " + numberOfGuess + " Attemps"
+      );
+      return;
+    } else if (cell.getAttribute("data-state") == "1") {
+      displayHit(inputGuest);
+      shipsSunk++;
+      displayMessage("Congratulations You Destroyed " + shipsSunk + " Ship");
+    } else {
+      displayMiss(inputGuest);
+      displayMessage("You missed!!! Try Again!");
+    }
+  }
+
+  //   document.getElementById("input").value = " ";
 }
 
-
-function play(e){
-    e.preventDefault();
-   let  inputGuest = document.getElementById("input").value;
-
-  
-
-   let cell = document.getElementById(inputGuest);
-
-   if(cell.style.display == "none"){
-    cell.style.backgroundImage =="url(/assets/ship.png)"
-    console.log("on target");
-    document.getElementById(locationId).style.display = "block"
-   }else{
-    cell.style.backgroundImage =="url(/assets/miss.png)"
-    console.log("missed");
-   }
+function startGame() {
+  shipLocations =[];
+  loadShips();
+  var audio = new Audio("/assets/mixkit-trumpet-fanfare-2293.wav");
+  audio.play();
 }
 
 createGrid();
-loadShips();
---
+++ js/main1.js
@@ -0,0 +1,145 @@
+// ************
+// Model
+// ***********
+var model = {
+  rows: 7,
+  cols: 7,
+  shipsSunk: 0,
+  numberOfGuess: 0,
+  shipLocations: [],
+  y_Axis: ["A", "B", "C", "D", "E", "F", "G"],
+
+  table: document.getElementById("table"),
+  form: document.getElementById("inputForm"),
+  
+};
+
+// ************
+// View   //
+// ***********
+
+view = {
+  displayMessage: function (message) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = message;
+  },
+
+  displayHit: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+    var audio = new Audio("/assets/mixkit-martial-arts-fast-punch-2047.wav");
+    audio.play();
+  },
+
+  displayMiss: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+    var audio = new Audio(
+      "/assets/mixkit-technological-futuristic-hum-2133.wav"
+    );
+    audio.play();
+  },
+
+  displayWin: function (message) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = message;
+    var audio = new Audio(
+      "/assets/mixkit-stadium-crowd-light-applause-362.wav"
+    );
+    audio.play();
+  },
+
+  // Create imaginary grid on the board
+  createGrid: function () {
+    for (let i = 0; i < model.rows; i++) {
+      const tr = document.createElement("tr");
+      let x = model.y_Axis[i];
+      for (let j = 0; j < model.cols; j++) {
+        const cell = document.createElement("td");
+        cell.setAttribute("id", x + "" + j);
+        tr.appendChild(cell);
+      }
+      table.appendChild(tr);
+    }
+  },
+
+  // Display ship location on the board
+  loadShips: function () {
+    for (let i = 0; i < 15; i++) {
+      let randomRow = Math.floor(Math.random() * 7);
+      let randomCol = model.y_Axis[Math.floor(Math.random() * 7)];
+
+      let locationId = randomCol + "" + randomRow;
+
+      // make sure a  location does not repeat
+      if (model.shipLocations.includes(locationId) == false) {
+        model.shipLocations.push(locationId);
+      }
+      if (model.shipLocations.length == 4) {
+        return;
+      }
+
+      model.shipLocations.map((location) => {
+        document.getElementById(location).setAttribute("data-state", "1");
+        document.getElementById(location).style.backgroundColor = "red";
+        // displayHit(location)
+      });
+    }
+  },
+};
+
+
+
+
+
+// ***************
+// Controller Methods
+//****************** */
+
+var controller = {
+
+   play: function (e) {
+        e.preventDefault();
+        const inputGuest = document.getElementById("input").value;
+      
+        const cell = document.getElementById(inputGuest);
+      
+        if (cell.className == "miss") {
+          view.displayMessage("You Seleted this cell already!!!  Try again ");
+        } else if (cell.className == "hit") {
+          view.displayMessage("You Destroyed this ship already!!!  Try again ");
+        } else {
+          model.numberOfGuess++;
+          if (model.shipsSunk == 2) {
+            view.displayHit(inputGuest);
+            view.displayWin(
+              "Congrats You Sinked 3 ships after " + numberOfGuess + " Attemps"
+            );
+            return;
+          } else if (cell.getAttribute("data-state") == "1") {
+            view.displayHit(inputGuest);
+            shipsSunk++;
+            view.displayMessage("Congratulations You Destroyed " + shipsSunk + " Ship");
+          } else {
+            view.displayMiss(inputGuest);
+            view.displayMessage("You missed!!! Try Again!");
+          }
+        }
+      
+        //   document.getElementById("input").value = " ";
+      },
+      
+      startGame: function () {
+        // shipLocations = [];
+        view.loadShips();
+        var audio = new Audio("/assets/mixkit-trumpet-fanfare-2293.wav");
+        audio.play();
+      }
+      
+}
+
+model.form.addEventListener("submit", controller.play);
+
+document.getElementById("button").onclick = controller.startGame;
+
+view.createGrid();
